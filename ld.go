8d87e0479ff7daab72a7365e935e0a80397fe93a
James Friend <james@kitefire.com>
2023-12-02 22:08:27 -0500

    fix for incorrect ld args

--- ld.go
+++ ld.go
@@ -13,7 +13,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var ldArgs = []string{"-G 0", "-nostartfiles", "-nodefaultlibs", "-nostdinc", "-M"}
+var ldArgs = []string{"-G0", "-M"}
 
 func createLdScript(w *Wave) (io.Reader, error) {
 	t := `
